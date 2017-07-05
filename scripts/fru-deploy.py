c4c06a7b24cfdc8c93b84a66801f8c7e90fdc201
Henrique Silva <henrique.silva@lnls.br>
2017-07-05 15:30:14 -0300

    [scripts/*] Merge deploy scripts

--- scripts/fru-deploy.py
+++ scripts/fru-deploy.py
@@ -6,13 +6,17 @@ from subprocess import call
 import shutil
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-b','--board', required=True, choices=['afc','amc','fmc'], help='Board type')
 parser.add_argument('-s','--start', required=True, help='Starting Serial Number of FMC list')
 parser.add_argument('-e','--end', required=True, type=str, help='Ending Serial Number of FMC list')
-parser.add_argument('-d','--dir', type=str, default=os.getcwd()+'/fmc_bin', help='Output directory for binary files')
+parser.add_argument('-d','--dir', type=str, default=os.getcwd()+'/fmc_fru_bin', help='Output directory for binary files')
 args = parser.parse_args()
 
 sn_start = int(args.start)
 sn_end = int(args.end)
+
+if args.board == 'afc':
+    args.board = 'amc'
 
 #Create output dir
 if os.path.exists(args.dir):
@@ -21,4 +25,4 @@ os.makedirs(args.dir)
 
 for sn in range(sn_start, sn_end+1, 1):
     cur_sn = str(sn)
-    call(["../fmc_fru "+args.dir+"/fmc_250_"+cur_sn+".bin \""+cur_sn+"\""], shell=True)
+    call(["../"+args.board+"_fru "+args.dir+"/"+args.board+"_"+cur_sn+".bin \""+cur_sn+"\""], shell=True)
