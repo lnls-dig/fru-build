f5ab1b5f2908d34989dad51f7dd55b6638e46918
Henrique Silva <henrique.silva@lnls.br>
2018-03-27 16:30:02 -0300

    [scripts/fru-deploy] Include function to find and increment serial number
    Now the function finds the last group of number on the SN string and
    increments it

--- scripts/fru-deploy.py
+++ scripts/fru-deploy.py
@@ -4,6 +4,19 @@ import argparse
 from subprocess import call
 import shutil
 from datetime import datetime
+import re
+
+
+lastNum = re.compile(r'(?:[^\d]*(\d+)[^\d]*)+')
+#Code from Chris Olds @ http://code.activestate.com/recipes/442460/
+def increment(s):
+    """ look for the last sequence of number(s) in a string and increment """
+    m = lastNum.search(s)
+    if m:
+        next = str(int(m.group(1))+1)
+        start, end = m.span(1)
+        s = s[:max(end-len(next), start)] + next + s[end:]
+    return s
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b','--board', required=True, choices=['afc','amc','fmc','rtm'], help='Board type')
@@ -12,9 +25,6 @@ parser.add_argument('-e','--end', required=True, type=str, help='Ending Serial N
 parser.add_argument('-d','--dir', type=str, default=os.getcwd()+'/fru_bin', help='Output directory for binary files')
 parser.add_argument('-t','--time', type=str, default='01/01/2017', help='Board manufacturing date (DD/MM/YYYY)')
 args = parser.parse_args()
-
-sn_start = int(args.start)
-sn_end = int(args.end)
 
 if args.board == 'afc':
     args.board = 'amc'
@@ -28,6 +38,10 @@ if os.path.exists(args.dir):
     shutil.rmtree(args.dir)
 os.makedirs(args.dir)
 
-for sn in range(sn_start, sn_end+1, 1):
-    cur_sn = str(sn)
+cur_sn = args.start
+while True:
     call(["../{}_fru {}/{}.bin \"{}\" {}".format(args.board, args.dir, cur_sn, cur_sn, delta_min)], shell=True)
+    if cur_sn == args.end:
+        break
+    else:
+        cur_sn = increment(cur_sn)
